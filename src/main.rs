605099085e9043ad5867b5425d3c82abbd9374b2
zhangchuachua <zchuachuax@gmail.com>
2024-12-31 18:23:51 +0800

    feat: subcommand cp-json-path completed;
    cp-json-path 完成，并完善了报错机制；
    完成 replace_with 函数；可以指定路径修改 json 内容;

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::{
-    borrow::BorrowMut,
     fs::{self},
     path::{Component, Path, PathBuf},
 };
 
-use clap::{error::ErrorKind, CommandFactory, Parser, Subcommand};
-use log::{error, warn};
-use serde_json::{json, map::Entry, Value};
+use clap::{error::ErrorKind, ColorChoice, CommandFactory, Parser, Subcommand};
+use colored::Colorize;
+use log::{error, info, warn};
+use serde_json::{map::Entry, Value};
 use serde_json_path::JsonPath;
 
+// TODO 需要调整报错的逻辑，需要整体性
 #[derive(Parser)]
-#[command(name = "File Copier", version, about, long_about = None)]
+#[command(name = "JSON Helper", version, about, long_about = None, color = ColorChoice::Always)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -48,6 +49,10 @@ enum Commands {
 }
 
 fn main() {
+    // 在代码中设置 log 等级，kt 是当前的包名，意味开启所有级别的日志打印；
+    std::env::set_var("RUST_LOG", "kt");
+    env_logger::init();
+
     let cli = Cli::parse();
 
     match &cli.command {
@@ -66,7 +71,14 @@ fn main() {
             target,
             base_path,
         } => {
-            cp_json_path(source, json_path, target, base_path);
+            match cp_json_path(source, json_path, target, base_path) {
+                Ok(_) => {
+                    info!("Command cp-json-path success");
+                }
+                Err(e) => {
+                    error!("Command cp-json-path failed -- {}", e);
+                }
+            };
         }
     }
 }
@@ -145,65 +157,133 @@ fn normalize_path(path: &Path) -> PathBuf {
     tmp
 }
 
-fn cp_json_path(source: &str, json_path: &str, _: &Vec<String>, base_path: &Option<String>) {
+fn path_to_normalized_str(path: &Path) -> Option<String> {
+    let tmp = normalize_path(path);
+    tmp.to_str().map(|s| s.to_string())
+}
+
+fn cp_json_path(
+    source: &str,
+    json_path: &str,
+    target_json_path: &Vec<String>,
+    base_path: &Option<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let base_path = base_path.clone().unwrap_or_else(|| "".to_string());
 
     let source_path = Path::new(&base_path).join(source);
 
     {
         let mut cmd = Cli::command();
-        let formated_source_path = normalize_path(&source_path);
-        let displayed_source_path = formated_source_path.display();
+        let tmp_path = path_to_normalized_str(&source_path).unwrap();
         if !source_path.exists() {
             cmd.error(
                 ErrorKind::InvalidValue,
-                format!(
-                    "source path not exists, source path: {}",
-                    displayed_source_path
-                ),
+                format!("source path not exists, source path: {}", tmp_path),
             )
             .exit();
         }
         if !source_path.is_file() {
             cmd.error(
                 ErrorKind::InvalidValue,
-                format!(
-                    "source path is not a file, source path: {}",
-                    displayed_source_path
-                ),
+                format!("source path is not a file, source path: {}", tmp_path),
             )
             .exit();
         }
         if source_path.extension().unwrap().ne("json") {
             cmd.error(
                 ErrorKind::InvalidValue,
-                format!(
-                    "source path is not a json file, source path: {}",
-                    displayed_source_path
-                ),
+                format!("source path is not a json file, source path: {}", tmp_path),
             )
-            .exit();
+            .exit()
         }
     }
 
-    let source_content = fs::read_to_string(source_path).unwrap();
-    let mut source_json: Value = serde_json::from_str(&source_content).unwrap();
+    let source_content = fs::read_to_string(source_path)?;
+    // *json 默认是无序的，如果想要保持顺序那么就需要为 serde_json 添加 preserve_order 这个 features
+    let source_json: Value = serde_json::from_str(&source_content)?;
+
+    for str in target_json_path {
+        let target_path = Path::new(&base_path).join(str);
+        let target_content = fs::read_to_string(&target_path)?;
+        let target_json: Value = serde_json::from_str(&target_content)?;
+        let target_path_str = path_to_normalized_str(&target_path).unwrap();
+
+        {
+            let mut cmd = Cli::command();
+            if !target_path.exists() {
+                cmd.error(
+                    ErrorKind::InvalidValue,
+                    format!("source path not exists, source path: {}", target_path_str),
+                )
+                .exit();
+            }
+            if !target_path.is_file() {
+                cmd.error(
+                    ErrorKind::InvalidValue,
+                    format!(
+                        "source path is not a file, source path: {}",
+                        target_path_str
+                    ),
+                )
+                .exit();
+            }
+            if target_path.extension().unwrap().ne("json") {
+                cmd.error(
+                    ErrorKind::InvalidValue,
+                    format!(
+                        "source path is not a json file, source path: {}",
+                        target_path_str
+                    ),
+                )
+                .exit();
+            }
+        }
+
+        let ret: Value = replace_with(json_path, target_json, &mut |json_pointer, _| {
+            if let Some(v) = source_json.pointer(json_pointer) {
+                Some(v.clone())
+            } else {
+                None
+            }
+        })?;
+
+        let json_result = serde_json::to_string_pretty(&ret)?;
+
+        fs::write(&target_path, json_result).map(|_| {
+            info!("target_path: {} is finished!", target_path_str);
+        })?;
+    }
+
+    Ok(())
 }
 
-fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
+fn replace_with<F: FnMut(&str, Value) -> Option<Value>>(
     replace_path: &str,
     mut value: Value,
-    mut func: F,
-) -> Value {
+    func: &mut F,
+) -> Result<Value, clap::error::Error> {
+    let mut cmd = Cli::command();
     // replace_path = "$.meta.*";
-    let json_path = JsonPath::parse(replace_path).expect("valid JSON Path");
-    // locs = ["/meta/title", "/meta/desc", "/meta/keywords"];
+    let json_path = JsonPath::parse(replace_path).map_err(|e| {
+        cmd.error(
+            ErrorKind::ValueValidation,
+            format!(
+                "This path ({}) is not a valid path; \n Parse msg: {}",
+                replace_path.green(),
+                e
+            ),
+        )
+    })?;
+
+    // pointers = ["/meta/title", "/meta/desc", "/meta/keywords"];
     let pointers = json_path
         .query_located(&value)
         .locations()
-        .map(|l| l.to_json_pointer()) // 转换成 json_pointer 后就把 query_located 消费了，对 value 的不可变借用也就消费了，所以后面可以对 value 可变借用；
+        // *转换成 json_pointer 后就把 query_located 消费了，对 value 的不可变借用也就消费了，所以后面可以对 value 可变借用；
+        .map(|l| l.to_json_pointer())
         .collect::<Vec<String>>();
 
+    // "/meta/title"
     for pointer in pointers {
         // ["meta", "title"]
         let path_vec = pointer
@@ -216,6 +296,7 @@ fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
         let mut target = &mut value;
 
         for index in 0..path_vec.len() {
+            // meta, title
             let key = path_vec[index];
             let is_last = index == last_index;
             let target_once = target;
@@ -225,13 +306,13 @@ fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
                     if is_last {
                         if let Entry::Occupied(mut e) = obj.entry(key) {
                             let v = e.insert(Value::Null);
-                            if let Some(res) = func(&path_vec, v) {
+                            if let Some(res) = func(&pointer, v) {
                                 e.insert(res);
                             } else {
                                 e.remove();
                             }
                         } else {
-                            warn!("这个路径 ({:?}) 将会获取到空 entry 跳过", path_vec);
+                            warn!("这个路径 ({}) 将会获取到空 entry, 跳过该路径！", &pointer);
                         }
                         None
                     } else {
@@ -239,8 +320,10 @@ fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
                             Some(tmp)
                         } else {
                             // TODO 对象不存在时是否新建一个对象
-                            error!("这个路径 ({:?}) 无法获取到正确的对象!", path_vec);
-                            panic!();
+                            return Err(cmd.error(
+                                ErrorKind::InvalidValue,
+                                format!("这个路径 ({}) 无法获取到正确的对象!", &pointer),
+                            ));
                         }
                     }
                 }
@@ -249,7 +332,7 @@ fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
                     if let Ok(i) = key.parse::<usize>() {
                         if is_last {
                             let v = std::mem::replace(&mut arr[i], Value::Null);
-                            if let Some(res) = func(&path_vec, v) {
+                            if let Some(res) = func(&pointer, v) {
                                 arr[i] = res;
                             } else {
                                 arr.remove(i);
@@ -260,16 +343,20 @@ fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
                                 Some(tmp)
                             } else {
                                 // TODO 对象不存在时是否新建一个对象
-                                error!("这个路径 ({:?}) 无法获取到正确的数组!", path_vec);
-                                panic!();
+                                return Err(cmd.error(
+                                    ErrorKind::InvalidValue,
+                                    format!("这个路径 ({}) 无法获取到正确的数组!", &pointer),
+                                ));
                             }
                         }
                     } else {
-                        error!(
-                            "这个路径({:?})中的索引无法解析为 usize 导致无法获取到正确的数组!",
-                            path_vec
-                        );
-                        panic!();
+                        return Err(cmd.error(
+                            ErrorKind::InvalidValue,
+                            format!(
+                                "这个路径({:?})中的索引无法解析为 usize 导致无法获取到正确的数组!",
+                                &pointer
+                            ),
+                        ));
                     }
                 }
                 _ => None,
@@ -283,7 +370,7 @@ fn replace_with<F: FnMut(&Vec<&str>, Value) -> Option<Value>>(
         }
     }
 
-    value
+    Ok(value)
 }
 
 #[test]
